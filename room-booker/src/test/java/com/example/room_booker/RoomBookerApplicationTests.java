bbfe2383e7ece455805431a011daf7f4d4196ff4
Bartosz Michalski <bartosz.michalski777@gmail.com>
2025-05-11 16:46:40 +0200

    chore: add application boilerplate

---
+++ room-booker/src/test/java/com/example/room_booker/RoomBookerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.room_booker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoomBookerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
