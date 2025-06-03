81081f01f5b0428e105aef3961d68c6b8e942b26
Bartosz Michalski <bartosz.michalski777@gmail.com>
2025-06-03 22:44:40 +0200

    Add component adnotation

--- room-booker/src/main/java/com/example/center/use_case/CenterGetIfBelongsToUser.java
+++ room-booker/src/main/java/com/example/center/use_case/CenterGetIfBelongsToUser.java
@@ -1,10 +1,12 @@
 package com.example.center.use_case;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 import com.example.center.entity.CenterEntity;
 import com.example.common.exception.PermissionDeniedException;
 
+@Component
 public class CenterGetIfBelongsToUser {
     @Autowired
     private CenterGetById centerGetById;
